806dbfc87c2fa407a521bbe343867f841107a2b7
A-Mosher <adam.l.mosher@gmail.com>
2021-09-02 21:18:38 -0400

    added next.config.js file to handle some images. adding an image in the header component.

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,14 @@
+import Image from "next/image";
+
 export default function Header() {
     return (
         <header className=''>
             <h1>This is the header</h1>
+            <Image
+            className="object-contain"
+            src="https://links.papareact.com/au6"
+            width={200}
+            height={100} />
         </header>
-    )
+    );
 }
